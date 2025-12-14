ea91a1227802591d4407b4520c56122d4ba3bc73
hiteshgit30 <49584546+hiteshgit30@users.noreply.github.com>
2025-12-14 13:05:09 +0000

    launch app rest api

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-y$%4)f@vqb2hmltr3#w%z1p=yaw(!jlru3qjm$%ccy1yx73!=a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
